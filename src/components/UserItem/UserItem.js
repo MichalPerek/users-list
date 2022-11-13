03961e82cd03049df93aba341e9df133e2c3cae9
mperek <michal.perek@ringieraxelspringer.pl>
2022-11-13 15:35:50 +0100

    exported user item backdrop to separate component

--- src/components/UserItem/UserItem.js
+++ src/components/UserItem/UserItem.js
@@ -1,44 +1,27 @@
 import React, {useState} from 'react';
 import {
-    Avatar,
-    Backdrop,
     Card,
-    Chip,
     Container,
     Grid,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Stack
 } from "@mui/material";
-import PersonIcon from '@mui/icons-material/Person';
-import EmailIcon from '@mui/icons-material/Email';
-import Grid3x3Icon from '@mui/icons-material/Grid3x3';
-import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
-import Typography from "@mui/material/Typography";
-import GradeIcon from '@mui/icons-material/Grade';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import CreditScoreIcon from '@mui/icons-material/CreditScore';
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
-import Button from "@mui/material/Button";
 import {useDispatch, useSelector} from "react-redux";
 import {removeFiredUser, setFiredUser} from "../usersSlice";
 import UserBio from "./UserBio";
 import UserPersonalData from "./UserPersonalData";
 import UserWorkData from "./UserWorkData";
 import UserActions from "./UserActions";
+import UserItemBackdrop from "./UserItemBackdrop";
 
 
 const UserItem = (props) => {
 
-    const [open, setOpen] = useState(false);
+    const [backdropOpen, setBackdropOpen] = useState(false);
     const [toggleMode, setToggleMode] = useState('')
     const [backdropMsg, setBackdropMsg] = useState('')
 
 
     const handleToggle = (toggleType) => {
-        setOpen(!open);
+        setBackdropOpen(!backdropOpen);
         setToggleMode(toggleType)
 
         if (toggleMode && toggleMode === 'fire') {
@@ -50,8 +33,8 @@ const UserItem = (props) => {
 
         }
     };
-    const handleClose = () => {
-        setOpen(false);
+    const handleCloseBackdrop = () => {
+        setBackdropOpen(false);
     };
 
     const handleExtendButtonCard = () => {
@@ -75,7 +58,9 @@ const UserItem = (props) => {
 
     const userAge = props.userData.dob.age
     const userEmail = props.userData.email
-    const userImg = props.userData.picture.large ? props.userData.picture.large : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdDXECBv76wa78obNrJNqayP3o7cy4RaZNg_l_YuhSzP6qoWuHr6BTtn8JgNuHFVmSaf4&usqp=CAU"
+    const userImg = props.userData.picture.large ?
+        props.userData.picture.large :
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdDXECBv76wa78obNrJNqayP3o7cy4RaZNg_l_YuhSzP6qoWuHr6BTtn8JgNuHFVmSaf4&usqp=CAU"
     const userId = props.userData.userId
 
     const userLabelName = props.userData.name.title + " " + props.userData.name.first + " " + props.userData.name.last;
@@ -98,72 +83,39 @@ const UserItem = (props) => {
                 justifyContent="center"
                 alignItems="flex-start">
 
-                <UserBio userLabelName={userLabelName} userImg={userImg}/>
-                <UserPersonalData userLabelName={userLabelName} userEmail={userEmail} userLabelId={userLabelId}
-                                  userLabelAge={userLabelAge}/>
+                <UserBio
+                    userLabelName={userLabelName}
+                    userImg={userImg}
+                />
+                <UserPersonalData
+                    userLabelName={userLabelName}
+                    userEmail={userEmail}
+                    userLabelId={userLabelId}
+                    userLabelAge={userLabelAge}
+                />
 
-                <UserWorkData userGradeLabel={userGradeLabel} userAttendanceLabel={userAttendanceLabel}
-                              userEvaluationLabel={userEvaluationLabel} userSalaryLabel={userSalaryLabel}/>
+                <UserWorkData
+                    userGradeLabel={userGradeLabel}
+                    userAttendanceLabel={userAttendanceLabel}
+                    userEvaluationLabel={userEvaluationLabel}
+                    userSalaryLabel={userSalaryLabel}
+                />
 
-
-                <UserActions extendBtnHanlder={handleExtendButtonCard} fireBtnHandler={handleFireButtonCard}  />
+                <UserActions
+                    extendBtnHanlder={handleExtendButtonCard}
+                    fireBtnHandler={handleFireButtonCard}
+                />
 
 
             </Grid>
 
-            <Backdrop
-                sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}}
-                open={open}
-                onClick={handleClose}
-            >
-                <Container
-                    maxWidth="sm">
-
-                    <Card>
-                        <Typography
-                            component="h1"
-                            variant="h6"
-                            align="center"
-                            color="text.primary"
-                            sx={{
-                                margin: '1rem'
-                            }}
-
-                        >
-                            {backdropMsg} <b>{userLabelName}</b> ?
-                        </Typography>
-
-                        <Stack
-                            sx={{p: 2}}
-                            direction="column"
-                            spacing={1}
-                            justifyContent="center"
-                            alignItems="center"
-                        >
-
-                            <Avatar alt={userLabelName} src={userImg}
-                                    sx={{
-                                        width: '100px',
-                                        height: '100px',
-                                        border: 'solid 1px black',
-                                        margin: '1rem'
-                                    }}/>
-                            <Stack
-                                sx={{p: 2}}
-                                direction="row"
-                                spacing={2}
-                                justifyContent="center"
-                            >
-                                <Button variant="contained" color="error"
-                                        onClick={handleFireButtonBackdrop}>Fire</Button>
-                                <Button variant="contained" color="success">Don't fire</Button>
-                            </Stack>
-                        </Stack>
-                    </Card>
-
-                </Container>
-
-            </Backdrop>
+            <UserItemBackdrop
+                userImg={userImg}
+                userLabelName={userLabelName}
+                handleCloseBackdrop={handleCloseBackdrop}
+                fireBtnHandler={handleFireButtonBackdrop}
+                backdropOpen={backdropOpen}
+            />
 
         </Card>
     </Container>);
---
+++ src/components/UserItem/UserItemBackdrop.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {Avatar, Backdrop, Card, Container, Stack} from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import {useState} from 'react';
+
+const UserItemBackdrop = (props) => {
+
+    const backdropOpen = props.backdropOpen
+    const userLabelName = props.userLabelName
+    const userImg = props.userImg
+    const handleCloseBackdrop = props.handleCloseBackdrop
+    const fireBtnHandler = props.handleFireButtonBackdrop
+
+    return (
+        <Backdrop
+            sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}}
+            open={backdropOpen}
+            onClick={handleCloseBackdrop}
+        >
+            <Container
+                maxWidth="sm">
+
+                <Card>
+                    <Typography
+                        component="h1"
+                        variant="h6"
+                        align="center"
+                        color="text.primary"
+                        sx={{
+                            margin: '1rem'
+                        }}
+
+                    >
+                        <b>{userLabelName}</b> ?
+                    </Typography>
+
+                    <Stack
+                        sx={{p: 2}}
+                        direction="column"
+                        spacing={1}
+                        justifyContent="center"
+                        alignItems="center"
+                    >
+
+                        <Avatar alt={userLabelName} src={userImg}
+                                sx={{
+                                    width: '100px',
+                                    height: '100px',
+                                    border: 'solid 1px black',
+                                    margin: '1rem'
+                                }}/>
+
+                        <Stack
+                            sx={{p: 2}}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                            <Button
+                                variant="contained"
+                                color="error"
+                                onClick={fireBtnHandler}
+                            >
+                                Fire
+                            </Button>
+
+                            <Button
+                                variant="contained"
+                                color="success"
+                            >
+                                Don't fire
+                            </Button>
+
+                        </Stack>
+                    </Stack>
+                </Card>
+
+            </Container>
+
+        </Backdrop>
+    );
+};
+
+export default UserItemBackdrop;
