eb1f3d826eab0bb0858f5f6dcd0541b040384193
mperek <michal.perek@ringieraxelspringer.pl>
2022-11-24 23:40:13 +0100

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
 import React from 'react'
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import UsersList from "./components/UsersList";
-import Footer from "./components/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/MainPanel/Hero";
+import UsersList from "./components/MainPanel/UsersList";
+import Footer from "./components/MainPanel/Footer";
 import NavigationFabs from "./components/NavigationFabs";
+import {Route, Routes} from 'react-router-dom'
+import MainPanel from "./pages/MainPanel";
+import Login from "./pages/Login"
+import NotFound from "./pages/NotFound";
 
 function App() {
     return (
-        <div className="App">
-            <Navbar/>
-            <NavigationFabs/>
-            <Hero/>
-            <UsersList/>
-            <Footer/>
-        </div>
+        <Routes>
+            <Route path ='/main' element ={<MainPanel/>}/>
+            <Route path ='/login' element = {<Login/>} />
+            <Route path ='/*' element = {<NotFound/>} />
+
+
+        </Routes>
     );
 }
 
